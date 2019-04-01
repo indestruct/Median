f8991607e962eedd780e831417eae2d6898a10ee
indestruct <mitkovulkov@gmail.com>
2019-04-01 13:54:42 +0300

    Added automated tests

--- Median.h
+++ Median.h
@@ -19,6 +19,7 @@
 #include <queue>
 #include <vector>
 
+template <class T>
 class Median
 {
 public:
@@ -26,14 +27,14 @@ public:
     virtual ~Median();
 
     // Insets a number to the median data structure
-    void InsertNumber(const int32_t value);
+    void InsertNumber(const T value);
 
     // Calculates the median for the current provided set of numbers
     double GetMedian();
 
 private:
-    typedef std::priority_queue <int32_t, std::vector<int32_t>, std::less<int32_t>>    MaxPriorityQueueT;
-    typedef std::priority_queue <int32_t, std::vector<int32_t>, std::greater<int32_t>> MinPriorityQueueT;
+    typedef std::priority_queue <T, std::vector<T>, std::less<T>>    MaxPriorityQueueT;
+    typedef std::priority_queue <T, std::vector<T>, std::greater<T>> MinPriorityQueueT;
 
     MaxPriorityQueueT m_maxPrioQueue; // Buffer for the elements that are less or equal the median
     MinPriorityQueueT m_minPrioQueue; // Buffer for the elements that are greater or equal the median
@@ -42,5 +43,109 @@ private:
     void BalanceQueues();
 };
 
+//
+// Code below contains the implementation of Median class. The implementation
+// is optimized to find fast the median of a set of numbers, by keeping
+// the elements in almost sorted order, in two priority queues - one for the
+// elements less or equal the median (left side of the median), the second
+// for the elements greater or equal the median (right side of the median).
+//
+// The queue that stores the left side of the median is sorted by the maximum
+// element, and the queue that stores the right side of the median is sorted
+// by the minimum element.
+//
+
+template <class T>
+Median<T>::Median()
+{
+    static_assert(std::is_arithmetic<T>::value, "The type is not an arithmetic type.");
+}
+
+template <class T>
+Median<T>::~Median()
+{
+}
+
+// Insert the number to the left or to the right priority queue
+// and balance them if after insertion they become unbalanced
+template <class T>
+void Median<T>::InsertNumber(const T value)
+{
+    if ((!m_maxPrioQueue.empty()) && (value > m_maxPrioQueue.top()))
+    {
+        m_minPrioQueue.push(value);
+    }
+    else
+    {
+        m_maxPrioQueue.push(value);
+    }
+
+    // Balance the queues if after insertion they become unbalanced
+    BalanceQueues();
+}
+
+// Return the median value based on the current set of numbers
+template <class T>
+double Median<T>::GetMedian()
+{
+    // Lambda for calculating if the set length is even
+    auto isLengthEven = [&](){return ((m_minPrioQueue.size() + m_maxPrioQueue.size()) % 2) == 0;};
+
+    double currentMedian = 0.0;
+
+    if ((m_minPrioQueue.size() == 0) && (m_maxPrioQueue.size() == 0))
+    {
+        throw "Cannot get median of empty set.";
+    }
+
+    // If the length is even, the median is calculated as the
+    // and if it is odd,
+    if (isLengthEven())
+    {
+        currentMedian = ((m_maxPrioQueue.top() + m_minPrioQueue.top()) / 2.0);
+    }
+    else
+    {
+        if (m_maxPrioQueue.size() > m_minPrioQueue.size())
+        {
+            currentMedian = static_cast<double> (m_maxPrioQueue.top());
+        }
+        else
+        {
+            currentMedian = static_cast<double> (m_minPrioQueue.top());
+        }
+    }
+
+    return currentMedian;
+}
+
+// Check if the queues are unbalanced and balance them by moving element
+// from the queue with more elements to the queue with less elements
+template <class T>
+void Median<T>::BalanceQueues()
+{
+    auto isUnbalanced = [](size_t leftSize, size_t rightSize){return (leftSize - rightSize) > 1;};
+
+    // Check if the max queue has more elements than the min queue,
+    // and if so, move element from max to the min queue
+    if ((m_maxPrioQueue.size() > m_minPrioQueue.size()) &&
+        (isUnbalanced(m_maxPrioQueue.size(), m_minPrioQueue.size())))
+    {
+        auto maxValue = m_maxPrioQueue.top();
+        m_maxPrioQueue.pop();
+        m_minPrioQueue.push(maxValue);
+    }
+
+    // Check if the min queue has more elements than the max queue,
+    // and if so, move element from min to the max queue
+    if ((m_minPrioQueue.size() > m_maxPrioQueue.size()) &&
+        (isUnbalanced(m_minPrioQueue.size(), m_maxPrioQueue.size())))
+    {
+        auto minValue = m_minPrioQueue.top();
+        m_minPrioQueue.pop();
+        m_maxPrioQueue.push(minValue);
+    }
+}
 
 #endif //MEDIAN_H
+
--- test.cpp
+++ test.cpp
@@ -1,40 +1,60 @@
+//
+// Author: mitkovulkov@gmail.com (Mitko Valkov)
+// Created on 29.03.19.
+//
+// This file contains the Median class test cases, which are executed
+// automatically.
+//
+
 #include <iostream>
+#include <tuple>
+#include <vector>
+
 #include "Median.h"
 
+std::vector<std::tuple<std::string, std::vector<int32_t>, double>> testCases =
+{
+        {"Empty set", {}, 0.0},
+        {"Single element set", {2}, 2.0},
+        {"Two element set", {3,2}, 2.5},
+        {"Three element set", {4,2,8}, 4.0}, // 2 4 8
+        {"Ten element set", {6,1,5,6,6,8,2,7,4,3}, 5.5}, // 1 2 3 4 5  6 6 6 7 8
+        {"Same element set", {5,5,5,5,5}, 5.0},
+};
+
 int main() {
-    Median newMedian;
 
-    try
-    {
-        auto res = newMedian.GetMedian();
+    for (auto &test : testCases) {
+        Median<int32_t> testMedian;
+
+        for (const auto &num : std::get<1>(test)) {
+            testMedian.InsertNumber(num);
+        }
+
+        try {
+            auto result = testMedian.GetMedian();
+
+            if (result == std::get<2>(test))
+            {
+                std::cout << "Test " << std::get<0>(test) << " passed" << std::endl;
+            }
+            else
+            {
+                std::cout << "Test " << std::get<0>(test) << " failed" << std::endl;
+            }
+        }
+        catch (const char *exception) {
+            std::cout << exception << std::endl;
+            if (0 == std::get<1>(test).size())
+            {
+                std::cout << "Test " << std::get<0>(test) << " passed" << std::endl;
+            }
+            else
+            {
+                std::cout << "Test " << std::get<0>(test) << " failed" << std::endl;
+            }
+        }
     }
-    catch (const char* exception)
-    {
-        std::cout << exception << std::endl;
-    }
-
-
-    newMedian.InsertNumber(1);
-
-    std::cout << "Median is " << newMedian.GetMedian() << "\n";
-
-    newMedian.InsertNumber(6);
-
-    std::cout << "Median is " << newMedian.GetMedian() << "\n";
-
-    newMedian.InsertNumber(4);
-
-    std::cout << "Median is " << newMedian.GetMedian() << "\n";
-
-    newMedian.InsertNumber(3);
-    newMedian.InsertNumber(2);
-    newMedian.InsertNumber(5);
-    newMedian.InsertNumber(3);
-    newMedian.InsertNumber(3);
-
-    // 1 2 3 3 3 4 5 6
-
-    std::cout << "Median is " << newMedian.GetMedian() << "\n";
 
     return 0;
 }
